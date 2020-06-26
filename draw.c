3318851010b2abb0adc7769f2a0ec74af6da3a98
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-06-25 20:32:34 -0500

    now using explicit struct initialization

--- draw.c
+++ draw.c
@@ -3,9 +3,9 @@
 int main()
 {
 	alloc_bitmap();
-	struct Triangle t = { (union Vertice) {100, 100},
-			 (union Vertice) {400, 400},
-			 (union Vertice) {300, 150} };
+	struct Triangle t = { .v1 = {100, 100},
+			 .v2 = {400, 400},
+			 .v3 = {300, 150} };
 	draw_triangle(&t);
 	write_bmp();
 	free_bitmap();
